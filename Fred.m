ffa2ab7b546521c321091af273c104821acaa4ac
cmoroz1 <cmoroz1@tulane.edu>
2016-06-06 10:34:26 -0500

    Added average frequency to Fred

--- Fred.m
+++ Fred.m
@@ -14,6 +14,13 @@ lambda_d = 20;
 theta0 = [0:-1/n:-1+1/n 0];
 
 
-[T,Y] = ode45(@oscRHS,[0 3],theta0,[],n,m,alphaf,omega,omegaf,Aa,Ad,lambda_a,lambda_d);
+[T,Y] = ode45(@oscRHS,[0 30],theta0,[],n,m,alphaf,omega,omegaf,Aa,Ad,lambda_a,lambda_d);
 toc
-plot(T,sin(2*pi*Y))
+%plot(T,sin(2*pi*Y))
+
+for i=1:20
+    h=T(size(T,1)-i+1)-T(size(T,1)-i);
+    A=(Y(size(Y,1)-i+1,:)-Y(size(Y,1)-i,:))/h;
+    B(i)=mean(A);
+end
+avgFreq=mean(B)
