ce5154f67dae004a2e6c90ded2d1d5fb7cc3218d
cmoroz1 <cmoroz1@tulane.edu>
2016-06-17 11:31:43 -0500

    small changes to NeuralModel.m

--- NeuralModel.m
+++ NeuralModel.m
@@ -1,4 +1,9 @@
 tic
+
+close all;
+clear;
+clc;
+
 G_R = 3.5; %Resting Conductance
 G_T = [0.875, 0.35, 3.5]; %Tonic Excitatory Conductance [E,L,C]
 G_0 = [15, 35]; %Maximal Synaptic Conductance of Intersegmental Connection
@@ -9,17 +14,17 @@ G_f = 1; %Maximal synaptic conductance of EC connections
 V_synec = [1, -1]; %Synaptic reversal potential for EC connections [excitatory, inhibatory]
 sigma = 0.05; %Parameter for smooth threshold function
 
-Aa = 0.0004;
-Ad = 0.0002;
-lambda_a = 4;
+Aa = 0.006;
+Ad = 0.0004;
+lambda_a = 0.75;
 lambda_d = 4;
-omega_f=0.7407; %Forcing frequency
+omega_f = 1/1.35; %Forcing frequency
 
-n = 24; %Number of oscillators
+n = 2; %Number of oscillators
 m = 0; %Forcing position
 dt = 0.001; %Time step
 
-alpha_f = 4; %Forcing strength
+alpha_f = 5; %Forcing strength
 
 v_0 = zeros(6*n+1,1); %Initial Conditions
 %v_0 = (2*rand(6*n+1,1))-1;
@@ -30,7 +35,7 @@ alpha_r = CouplingFunction(n,m,Aa,Ad,lambda_a,lambda_d,sigma);
 %alpha_r is the strength of the connections for different r
 %rows are of r and columns are [L to C, E to C, other]
 
-[T,Y] = ode45(@neuralFunc,(0:dt:30),v_0,options,n,m,G_R,G_T,G_0,V_syn,G_f,V_synec,sigma,alpha_f,alpha_r,omega_f);
+[T,Y] = ode45(@neuralFunc,(0:dt:5),v_0,options,n,m,G_R,G_T,G_0,V_syn,G_f,V_synec,sigma,alpha_f,alpha_r,omega_f);
 
 % plot(T,Y(:,1),'b');
 % hold on
