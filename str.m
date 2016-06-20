90c66e91a51fdfed2f85cfa78ed2aa384c8655bc
cmoroz1 <cmoroz1@tulane.edu>
2016-06-20 10:08:40 -0500

    Modified str function to work for ascending and descending

--- str.m
+++ str.m
@@ -1,5 +1,12 @@
-function alpha = str(A,l,k)
-%generalized function for ascending OR descending coupling strength
-    alpha = A*exp(-abs(k)/l);
+function alpha = str(Aa,Ad,lambda_a,lambda_d,k)
+    %coupling strength function which returns strengths for ascending or
+    %descending connections
+    if (k < 0)
+        alpha = Aa*exp(-abs(k)/lambda_a);
+    elseif (k > 0)
+        alpha = Ad*exp(-abs(k)/lambda_d);
+    else
+        alpha = 1;  %Is either 0 or 1 depending on the model
+        %alpha = 0; %1 for NeuralModel, 0 for Phase Model
+    end
 end
-
