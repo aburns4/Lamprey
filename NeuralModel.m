35847cf83d102f064f18cf6c26037d40e04352e4
Andrea D Burns <Andrea!@Andrea-Burnss-MacBook-Pro.local>
2016-06-20 14:26:24 -0500

    Added plotting of phase differences

--- NeuralModel.m
+++ NeuralModel.m
@@ -14,13 +14,13 @@ G_f = 1; %Maximal synaptic conductance of EC connections
 V_synec = [1, -1]; %Synaptic reversal potential for EC connections [excitatory, inhibatory]
 sigma = 0.05; %Parameter for smooth threshold function
 
-Aa = 0.006;
-Ad = 0.0004;
-lambda_a = 0.75;
-lambda_d = 4;
-omega_f = 1/1.35; %Forcing frequency
+Aa = .0004;
+Ad = .0002;
+lambda_a = 4;%(-1*log(0.05))^(-1);
+lambda_d = 4;%(-1*log(0.02))^(-1);
+omega_f = 1; %Forcing frequency
 
-n = 30; %Number of oscillators
+n = 1; %Number of oscillators
 m = 0; %Forcing position
 dt = 0.001; %Time step
 
@@ -42,7 +42,8 @@ for r = 1:(2*n-1)
 end
     
 
-[T,Y] = ode45(@neuralFunc,(0:dt:4),v_0,options,n,m,G_R,G_T,G_0,V_syn,G_f,V_synec,sigma,alpha_f,alpha_r,omega_f);
+[T,Y] = ode45(@neuralFunc,(0:dt:10),v_0,options,n,m,G_R,G_T,G_0,V_syn,G_f,V_synec,sigma,alpha_f,alpha_r,omega_f);
+%[T,Y] = ode45(@neuralFunc,(0:dt:1.35),Y(end,:),options,n,m,G_R,G_T,G_0,V_syn,G_f,V_synec,sigma,alpha_f,alpha_r,omega_f);
 
 % plot(T,Y(:,1),'b');
 % hold on
@@ -59,5 +60,34 @@ end
 %axis([0 2.7 -1 1]);
 figure(1)
 plot(T,Y(:,1:6*n));
+
+figure(2)
+for i = 1:(2*n)-8
+    E1 = Y(:,i); %first E cell, left side
+    L1 = Y(:,i+1);%first L cell, left side
+    C1 = Y(:,i+2);%first C cell, left side
+    E2 = Y(:,i+6);%second E cell, left side
+    L2 = Y(:,i+7);%second L cell, left side
+    C2 = Y(:,i+8);%second C cell, left side
+    
+    [A1, IA1] = max(E1);
+    [A2, IA2] = max(E2);
+    
+    [B1, IB1] = max(L1);
+    [B2, IB2] = max(L2);
+    
+    [D1, ID1] = max(C1);
+    [D2, ID2] = max(C2);
+    
+    Ediff = (IA2 - IA1)*dt;
+    Ldiff = (IB2 - IB1)*dt;
+    Cdiff = (ID2 - ID1)*dt;
+    scatter(T(IA2),Ediff);
+    hold on
+    scatter(T(IB2),Ldiff);
+    
+    scatter(T(ID2),Cdiff);
+    
+end
 
 toc
