13e1e9af8a1221793a755227dc149dcb25ed78a8
Michael Conrad <michaelconrad@Michaels-MacBook-Air-2.local>
2016-06-21 11:48:27 -0500

    find period and frequency for all segments

--- runFredV2.m
+++ runFredV2.m
@@ -5,8 +5,8 @@ clear;
 clc;
 
 t_0=0;
-t_f=20;
-dt = 0.001; %Time step
+t_f=76;
+dt = 0.0005; %Time step
 
 G_R = 3.5; %Resting Conductance
 G_T = [0.875, 0.35, 3.5]; %Tonic Excitatory Conductance [E,L,C]
@@ -20,7 +20,7 @@ sigma = 0.05; %Parameter for smooth threshold function
 
 omega_f = 1; %Forcing frequency
 
-n = 10; %Number of oscillators
+n = 5; %Number of oscillators
 m = 0; %Forcing position
 
 alpha_f = 5; %Forcing strength
@@ -38,69 +38,78 @@ alpha_r=[.05 1 .02];
 
 %plot(T,Y(:,1:6*n))
 
-% figure(1)
-% for i= 1:size(Y,2)
-%     if mod(i,6)==0
-%         plot(T,Y(:,i))
-%         hold on
-%     end
-% end
-% 
-% figure(2)
-% for i= 1:size(Y,2)-6
-%     if mod(i,6)==0
-%         plot(T,Y(:,i+1))
-%         hold on
-%     end
-% end
-% 
-% figure(3)
-% for i= 1:size(Y,2)-6
-%     if mod(i,6)==0
-%         plot(T,Y(:,i+2))
-%         hold on
-%     end
-% end
+figure(1)
+for i= 1:size(Y,2)
+    if mod(i,6)==0
+        plot(T,Y(:,i))
+        hold on
+    end
+end
+
+figure(2)
+for i= 1:size(Y,2)-6
+    if mod(i,6)==0
+        plot(T,Y(:,i+1))
+        hold on
+    end
+end
+
+figure(3)
+for i= 1:size(Y,2)-6
+    if mod(i,6)==0
+        plot(T,Y(:,i+2))
+        hold on
+    end
+end
 
 
 %CALCULATES THE PERIOD AND NATURAL FREQUENCY OF THE SYSTEM
-E_zeros=find(Y(1:end-1,1)<0 & Y(2:end,1)>=0);
-L_zeros=find(Y(1:end-1,2)<0 & Y(2:end,2)>=0);
-C_zeros=find(Y(1:end-1,3)<0 & Y(2:end,3)>=0);
-y1=E_zeros(2)-E_zeros(1);
-y2=L_zeros(2)-L_zeros(1);
-y3=C_zeros(2)-C_zeros(1);
-period = (y1+y2+y3)*dt/3;
-freq = 1/period;
-fprintf('The period is %f and the frequency is %f \n',period,freq);
+period = zeros(1,n);
+j=0;
+for i = 1:6:6*n
+    j=j+1;
+        
+        E_zeros=find(Y(1:end-1,i)<0 & Y(2:end,i)>=0);
+        L_zeros=find(Y(1:end-1,i+1)<0 & Y(2:end,i+1)>=0);
+        C_zeros=find(Y(1:end-1,i+2)<0 & Y(2:end,i+2)>=0);
+        y1=E_zeros(end)-E_zeros(end-1);
+        y2=L_zeros(end)-L_zeros(end-1);
+        y3=C_zeros(end)-C_zeros(end-1);
+        period(j) = (y1+y2+y3)*dt/3;
+        freq(j) = 1/period(j);
+end
+
+
+
+
 
 
 %Plots Phase Lag
-figure(2)
-for i = 1:(2*n)-8
-    E1 = Y(:,i); %first E cell, left side
-    L1 = Y(:,i+1);%first L cell, left side
-    C1 = Y(:,i+2);%first C cell, left side
-    E2 = Y(:,i+6);%second E cell, left side
-    L2 = Y(:,i+7);%second L cell, left side
-    C2 = Y(:,i+8);%second C cell, left side
-    
-    [A1, IA1] = max(E1);
-    [A2, IA2] = max(E2);
-    
-    [B1, IB1] = max(L1);
-    [B2, IB2] = max(L2);
-    
-    [D1, ID1] = max(C1);
-    [D2, ID2] = max(C2);
-    
-    Ediff = (IA2 - IA1)*dt;
-    Ldiff = (IB2 - IB1)*dt;
-    Cdiff = (ID2 - ID1)*dt;
-    scatter(T(IA2),Ediff);
-    hold on
-    scatter(T(IB2),Ldiff);
-    
-    scatter(T(ID2),Cdiff);
-    
-end
+% figure(2)
+% for i = 1:(2*n)-8
+%     E1 = Y(:,i); %first E cell, left side
+%     L1 = Y(:,i+1);%first L cell, left side
+%     C1 = Y(:,i+2);%first C cell, left side
+%     E2 = Y(:,i+6);%second E cell, left side
+%     L2 = Y(:,i+7);%second L cell, left side
+%     C2 = Y(:,i+8);%second C cell, left side
+%     
+%     [A1, IA1] = max(E1);
+%     [A2, IA2] = max(E2);
+%     
+%     [B1, IB1] = max(L1);
+%     [B2, IB2] = max(L2);
+%     
+%     [D1, ID1] = max(C1);
+%     [D2, ID2] = max(C2);
+%     
+%     Ediff = (IA2 - IA1)*dt;
+%     Ldiff = (IB2 - IB1)*dt;
+%     Cdiff = (ID2 - ID1)*dt;
+%     scatter(T(IA2),Ediff);
+%     hold on
+%     scatter(T(IB2),Ldiff);
+%     
+%     scatter(T(ID2),Cdiff);
+%     
+% end
