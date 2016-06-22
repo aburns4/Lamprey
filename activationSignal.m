38601884e3e9a101529989e9c8654d9fe88958c1
Michael Conrad <michaelconrad@Michaels-MacBook-Air-2.local>
2016-06-22 09:05:43 -0500

    added animation

--- activationSignal.m
+++ activationSignal.m
@@ -9,7 +9,7 @@ t_f=10;
 dt = 0.001; %Time step
 
 G_R = 3.5; %Resting Conductance
-G_T = [0.875, 0.35, 3.5]; %Tonic Excitatory Conductance [E,L,C]
+G_T = [0.875, 0.35*4.05, 3.5]; %Tonic Excitatory Conductance [E,L,C]
 G_0 = [15, 35]; %Maximal Synaptic Conductance of Intersegmental Connection
                 %[L to C, other]
 V_syn = [1, -1]; %Synaptic reversal potential for intersegmental connection
@@ -20,7 +20,7 @@ sigma = 0.05; %Parameter for smooth threshold function
 
 omega_f = 1; %Forcing frequency
 
-n = 5; %Number of oscillators
+n = 15; %Number of oscillators
 m = 0; %Forcing position
 
 alpha_f = 5; %Forcing strength
@@ -48,17 +48,34 @@ for i=1:n
         if(LeftECellMat(j,i)>0)
             LeftECellMat(j,i)=1;
         else
-            LeftECellMat(j,i)=0;
+            LeftECellMat(j,i)=.2;
         end
         if(RightECellMat(j,i)>0)
             RightECellMat(j,i)=1;
         else
-            RightECellMat(j,i)=0;
+            RightECellMat(j,i)=.2;
         end
     end
 end
 
-figure(1);
-plot(LeftECellMat);
-figure(2);
-plot(RightECellMat);
+% figure(1);
+% plot(LeftECellMat);
+% figure(2);
+% plot(RightECellMat);
+
+
+for t=1:length(T)
+    if mod(t,30)==0
+        figure(1)
+       
+        plot(LeftECellMat(t,:))
+        hold on
+        plot(-RightECellMat(t,:))
+        
+        axis([.9 n+.1 -1.1 1.1])
+        title(sprintf('Time = %.2f',t*dt));
+        pause(.09)
+        
+        clf('reset')
+    end
+end
